512518a9fcb7439b2a029c896e55f13ecacda58c
Slobodan Gajic <freelancerslobodan@gmail.com>
2024-02-21 13:04:32 +0100

    add dates

--- src/app/site/site.component.ts
+++ src/app/site/site.component.ts
@@ -153,13 +153,7 @@ export class SiteComponent {
     },
   ];
 
-  deliveryDates = [
-    '2024-02-22',
-    '2024-02-25',
-    '2024-02-27',
-    '2024-02-28',
-    '2024-02-29',
-  ];
+  deliveryDates = ['2024-03-01', '2024-03-02'];
 
   selectedAddress: Address | undefined;
   selectedOrder: Order | undefined;
