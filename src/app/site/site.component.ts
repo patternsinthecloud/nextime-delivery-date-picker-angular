2bc470d04a6c18c461384e6e2a2a254b99164c01
Slobodan Gajic <freelancerslobodan@gmail.com>
2024-02-27 12:53:48 +0100

    update demo page

--- src/app/site/site.component.ts
+++ src/app/site/site.component.ts
@@ -41,8 +41,15 @@ interface Order {
   shippingAddress: object;
   items: Item[];
   shippingOptionFilters: {
-    minDeliveryDate: string;
-    maxDeliveryDate: string;
+    minDeliveryDate?: string;
+    maxDeliveryDate?: string;
+  };
+}
+
+interface shippingData {
+  [key: number]: {
+    addresses: Address[];
+    orders: Order[];
   };
 }
 
@@ -55,6 +62,124 @@ export class SiteComponent {
   siteId: number | undefined;
   returnedObject?: ShippingDetails;
   showJson = false;
+
+  mockData: shippingData = {
+    8: {
+      addresses: [
+        {
+          name: '10 Sims Ave',
+          street: '10 Sims Ave',
+          street2: null,
+          city: 'Providence',
+          state: 'RI',
+          postalCode: '02909',
+          country: 'US',
+        },
+        {
+          name: '289 Buxton Street',
+
+          street: '289 Buxton Street',
+          city: 'North Smithfield',
+          state: 'RI',
+          country: 'US',
+          postalCode: '06277',
+        },
+        {
+          name: '8 Conifer Court',
+          street: '8 Conifer Court',
+          city: 'Coventry',
+          state: 'RI',
+          country: 'US',
+          postalCode: '02816',
+        },
+      ],
+      orders: [
+        {
+          OrderId: 1001,
+          shippingAddress: {},
+          items: [
+            {
+              id: 571386,
+              productId: '461150',
+              qty: 1,
+              regularPrice: 11.99,
+              salePrice: 11.99,
+            },
+            {
+              id: 840998,
+              productId: '426480',
+              qty: 1,
+              regularPrice: 20.0,
+              salePrice: 20.0,
+            },
+            {
+              id: 840999,
+              productId: '4026',
+              qty: 1,
+              regularPrice: 10.49,
+              salePrice: 10.49,
+            },
+          ],
+          shippingOptionFilters: {
+            minDeliveryDate: '2024-02-20',
+            maxDeliveryDate: '2024-03-20',
+          },
+        },
+        {
+          OrderId: 1002,
+          shippingAddress: {},
+          items: [
+            {
+              id: 840999,
+              productId: '4026',
+              qty: 1,
+              regularPrice: 10.49,
+              salePrice: 10.49,
+            },
+          ],
+          shippingOptionFilters: {
+            minDeliveryDate: '2024-02-20',
+            maxDeliveryDate: '2024-03-20',
+          },
+        },
+      ],
+    },
+    4: {
+      addresses: [
+        {
+          street: '2108 S Lamar Blvd',
+          city: 'Austin',
+          state: 'TX',
+          country: 'US',
+          postalCode: '78704',
+          name: 'Lamar Blvd',
+        },
+      ],
+      orders: [
+        {
+          OrderId: 641390,
+          shippingAddress: {},
+          items: [
+            {
+              id: 773779,
+              productId: '11042',
+              qty: 1,
+              regularPrice: 81.0,
+              salePrice: 76.95,
+            },
+            {
+              id: 946054,
+              productId: '11015',
+              qty: 1,
+              regularPrice: 13.99,
+              salePrice: 13.29,
+            },
+          ],
+          shippingOptionFilters: {},
+        },
+      ],
+    },
+  };
 
   products: {
     [number: number]: {
@@ -71,6 +196,14 @@ export class SiteComponent {
       image: 'assets/images/image3.png',
     },
     840999: {
+      name: 'Revive',
+      image: 'assets/images/image4.png',
+    },
+    946054: {
+      name: 'From the Earth',
+      image: 'assets/images/image3.png',
+    },
+    773779: {
       name: 'Revive',
       image: 'assets/images/image4.png',
     },
@@ -216,11 +349,11 @@ export class SiteComponent {
   ngOnInit(): void {
     this.route.paramMap.subscribe((params) => {
       this.siteId = +params.get('id')!;
-    });
 
-    this.selectedAddress = this.addresses[0];
-    this.selectedOrder = this.orders[0];
-    this.orderDate = this.orderDates[0];
-    this.selectedOrderDate = this.orderDate;
+      this.selectedAddress = this.mockData[this.siteId].addresses[0];
+      this.selectedOrder = this.mockData[this.siteId].orders[0];
+      this.orderDate = this.orderDates[0];
+      this.selectedOrderDate = this.orderDate;
+    });
   }
 }
